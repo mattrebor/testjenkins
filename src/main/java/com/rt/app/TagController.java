48568960ab27be87f25003ced8e6150a8b3b7aca
mattrebor <robert.tam+git@gmail.com>
2019-01-19 18:05:45 -0500

    Update TagController.java

--- src/main/java/com/rt/app/TagController.java
+++ src/main/java/com/rt/app/TagController.java
@@ -10,6 +10,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.servlet.ServletContext;
 
 /**
  * Handles requests for the application home page.
@@ -17,6 +20,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class TagController {
 	
+	@Autowired
+	ServletContext context;
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
@@ -24,7 +30,18 @@ public class TagController {
 	 */
 	@RequestMapping(value = "/tag", method = RequestMethod.GET)
 	public String tag(Locale locale, Model model) {
+		String tag_name = context.getInitParameter("tag_name");
+		if (tag_name != null) {
+
+			logger.info("tag_name |" + tag_name + "|");
+			model.addAttribute("tag_name", tag_name);
+		}
+		else {
+			tag_name = "NOT DEFINED";
+		}
+
 		return "tag";
 	}
 	
 }
+
