bd86e34fee63084583b77d24e15b38aff155fa0e
mattrebor <robert.tam+git@gmail.com>
2019-01-19 13:24:54 -0500

    Create TagController.java

---
+++ src/main/java/com/rt/app/TagController.java
@@ -0,0 +1,30 @@
+package com.rt.app;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class TagController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/tag", method = RequestMethod.GET)
+	public String tag(Locale locale, Model model) {
+		return "tag";
+	}
+	
+}
