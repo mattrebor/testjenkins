cdde303f04a8cc731d74c9e650aa59e5f987d522
Robert Tam <robert.tam+git@gmail.com>
2019-01-21 20:22:19 -0500

    Email

--- src/main/java/com/rt/app/TagController.java
+++ src/main/java/com/rt/app/TagController.java
@@ -18,7 +18,7 @@ import javax.servlet.ServletContext;
  * Handles requests for the application home page.
  */
 @Controller
-public class TagControllera {
+public class TagController {
 	
 	@Autowired
 	ServletContext context;
